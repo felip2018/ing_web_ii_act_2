17d857a3c10cdcd120fad897dac250698592ac34
felip2018 <felipegarxon@hotmail.com>
2022-11-12 15:55:35 -0500

    :+1:  initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ing-web-ii-act-2';
+  celsius: number = 0;
+  fahrenheit: number = 0;
+
+  ngOnInit() {
+    this.getFahrenheit();
+  }
+
+  getFahrenheit() {
+    console.log(`Grados celsius ${this.celsius}`);
+    this.fahrenheit = (this.celsius*(9/5))+32;
+  }
+
 }
